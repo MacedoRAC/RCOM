7d1eccc527e1512bea4750525868f4ccbae60197
Mário Macedo <andr.macedo1@gmail.com>
2014-10-02 13:56:39 +0100

    Aula 1 e 2
    Aula 2 ainda não está completa

---
+++ Aula1/noncanonical.c
@@ -0,0 +1,114 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+
+#define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+#define F 0x7e
+#define A 0x01
+#define C 0x03
+
+unsigned char SET[5];
+
+volatile int STOP=FALSE;
+
+int main(int argc, char** argv)
+{
+  int fd,c, res, numChar;
+    struct termios oldtio,newtio;
+    char buf[255];
+    char buf2[255];
+    char string[255];
+    int i, sum = 0, speed = 0;
+    
+    if ( (argc < 2) || 
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS4", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS4\n");
+      exit(1);
+    }
+
+    SET[0]=F;
+    SET[1]=A;
+    SET[2]=C;
+    SET[3]=SET[1]^SET[2];
+    SET[4]=F;
+
+
+  /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+
+
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(argv[1]); exit(-1); }
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+
+
+
+  /* 
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+    leitura do(s) próximo(s) caracter(es)
+  */
+
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+    
+    int counter = 0;
+ 
+    while(STOP==FALSE){
+      res = read(fd, buf, 1);
+      buf[res] = 0;
+      buf2[counter]= buf[0];
+      counter++;
+      printf(":%s:%d\n", buf, res);
+      if(buf[0] == 'z') STOP = TRUE;
+    }
+  /* 
+    O ciclo FOR e as instruções seguintes devem ser alterados de modo a respeitar 
+    o indicado no guião 
+  */
+
+
+    write(fd, buf2, strlen(buf2));
+   
+    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+
+
+
+    close(fd);
+    return 0;
+}
